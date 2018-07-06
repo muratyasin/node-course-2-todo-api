9771900620076c5de26411dcc9fa0f781c2fa505
ma3mac <ma3mac@m.local>
2018-07-06 14:37:47 +0300

    Add /todos/:id

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,8 +2,8 @@ var mongoose= require('mongoose');
 
 mongoose.Promise=global.Promise;
 
-//mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
-mongoose.connect('mongodb://heroku_t414lbrp:grtvkugducqc04vrh4ml9kktbv@ds229771.mlab.com:29771/heroku_t414lbrp');
+//mongoose.connect('mongodb://heroku_t414lbrp:grtvkugducqc04vrh4ml9kktbv@ds229771.mlab.com:29771/heroku_t414lbrp');
 //mongodb://heroku_t414lbrp:grtvkugducqc04vrh4ml9kktbv@ds229771.mlab.com:29771/heroku_t414lbrp
 module.exports = {mongoose};
