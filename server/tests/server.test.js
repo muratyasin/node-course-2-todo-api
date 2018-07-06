2ee2dd608055f08342b5792554dc3f26685c6059
ma3mac <ma3mac@m.local>
2018-07-06 11:26:08 +0300

    Add test cases for Get todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -6,10 +6,14 @@ const{app} = require ('./../server');
 
 const{Todo} = require ('./../models/todo');
 
+const {ObjectID} = require ('mongodb');
+
 const todos = [{
+  _id: new ObjectID(),
   text: 'First test todo'
 },
   {
+    _id: new ObjectID(),
   text: 'Second test todo'
   }];
 beforeEach( (done)=>{
@@ -72,6 +76,37 @@ describe('POST /todos', ()=>{
 
     });
   });
+
+  describe('GET /todos/:id', ()=>{
+    it('should return todo doc', (done)=>{
+      request(app)
+      .get(`/todos/${todos[0]._id.toHexString()}`)
+      .expect(200)
+      .expect((res)=>{
+        expect(res.body.todo.text).toBe(todos[0].text);
+      }).
+      end(done);
+
+    });
+
+    it('should return 404 if todo not found', (done)=>{
+      var hexId = new ObjectID().toHexString();
+      request(app)
+      .get(`/todos/${hexId}`)
+      .expect(404)
+      .end(done);
+
+    });
+
+    it('should return 404 if non object id found', (done)=>{
+      request(app)
+      .get('/todo/123')
+      .expect(404)
+      .end(done);
+
+    });
+
+  })
 
 
 });
