684653a27d4a4dcbcecbfd41afed422e92294bea
ma3mac <ma3mac@m.local>
2018-07-06 15:18:02 +0300

    Heroku setup

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -6,4 +6,6 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp')
 
 //mongoose.connect('mongodb://heroku_t414lbrp:grtvkugducqc04vrh4ml9kktbv@ds229771.mlab.com:29771/heroku_t414lbrp');
 //mongodb://heroku_t414lbrp:grtvkugducqc04vrh4ml9kktbv@ds229771.mlab.com:29771/heroku_t414lbrp
+
+
 module.exports = {mongoose};
